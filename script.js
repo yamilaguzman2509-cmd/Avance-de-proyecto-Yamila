37c8b4f215dcd392ff2dc2e527e0ac014e4fa882
Yamila Guzman <yamilaguzman2509@gmail.com>
2026-04-12 23:02:07 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,57 @@
+function agregar() {
+    let actividad = document.getElementById("actividad").value;
+    let tipo = document.getElementById("tipo").value;
+
+    if (actividad === "") {
+        alert("Escribe una actividad 💅");
+        return;
+    }
+
+    let fecha = new Date().toLocaleString();
+
+    let lista = document.getElementById("lista");
+    let item = document.createElement("li");
+
+    let texto = document.createElement("span");
+    texto.textContent = tipo + " | " + actividad + " | " + fecha;
+
+    // BOTÓN HECHO
+    let btnHecho = document.createElement("button");
+    btnHecho.textContent = "Hecho";
+
+    btnHecho.onclick = function () {
+        texto.style.textDecoration = "line-through";
+        texto.style.opacity = "0.6";
+
+        let confirmar = confirm("¿Deseas eliminar esta tarea?");
+        if (confirmar) {
+            lista.removeChild(item);
+        }
+    };
+
+    // BOTÓN PENDIENTE
+    let btnPendiente = document.createElement("button");
+    btnPendiente.textContent = "Pendiente";
+
+    btnPendiente.onclick = function () {
+        texto.style.textDecoration = "none";
+        texto.style.opacity = "1";
+    };
+
+    // BOTÓN ELIMINAR
+    let btnEliminar = document.createElement("button");
+    btnEliminar.textContent = "Eliminar";
+
+    btnEliminar.onclick = function () {
+        lista.removeChild(item);
+    };
+
+    item.appendChild(texto);
+    item.appendChild(btnHecho);
+    item.appendChild(btnPendiente);
+    item.appendChild(btnEliminar);
+
+    lista.appendChild(item);
+
+    document.getElementById("actividad").value = "";
+}
