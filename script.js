30ebcef71b667dbf746cbfbb34f521d382fca25c
Yamila Guzman <yamilaguzman2509@gmail.com>
2026-04-12 23:26:22 -0600

    Segundo commit

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function agregar() {
     let actividad = document.getElementById("actividad").value;
     let tipo = document.getElementById("tipo").value;
+    let lista = document.getElementById("lista");
 
-    if (actividad === "") {
-        alert("Escribe una actividad 💅");
+    // VALIDACIÓN MEJORADA
+    if (actividad.trim() === "") {
+        alert("Escribe una actividad valida por favor");
         return;
+    }
+
+    // LIMPIAR MENSAJE SI EXISTE
+    if (lista.textContent.includes("No hay actividades aún")) {
+        lista.innerHTML = "";
     }
 
     let fecha = new Date().toLocaleString();
 
-    let lista = document.getElementById("lista");
     let item = document.createElement("li");
-
     let texto = document.createElement("span");
+
     texto.textContent = tipo + " | " + actividad + " | " + fecha;
 
     // BOTÓN HECHO
@@ -54,4 +60,6 @@ function agregar() {
     lista.appendChild(item);
 
     document.getElementById("actividad").value = "";
+
+    console.log("Tarea agregada correctamente");
 }
